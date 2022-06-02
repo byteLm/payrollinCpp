f6bb1cd13549fa22d28809a00123ce9f78e4df86
Luccas Freitas <90070934+byteLm@users.noreply.github.com>
2022-06-02 00:05:12 -0300

    Create presidente.cpp

---
+++ classesBase/c3Presidente/presidente.cpp
@@ -0,0 +1,34 @@
+#include "c3Presidente.h"
+
+
+c3Presidente::c3Presidente(){
+    this->FormacaoMax = "";
+    this->codigo = codigo;
+    this->desig = desig;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->telefone = telefone;
+    this->dataIngresso = dataIngresso;
+    this->salario = salario;
+    this->areaSupervisao = "";
+};
+c3Presidente::c3Presidente(int codigo, string nome, string endereco, string telefone, string dataIngresso, float salario, int desig, string AreaSupervisao, string areaFormacao, string formMax){
+    this->FormacaoMax = formMax;
+    this->codigo = codigo;
+    this->desig = desig;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->telefone = telefone;
+    this->dataIngresso = dataIngresso;
+    this->salario = salario;
+    this->areaSupervisao = AreaSupervisao;
+    this->areaFormacao = areaFormacao;
+};
+void c3Presidente::setFormacaoMax(string FormacaoMax){
+    this->FormacaoMax = FormacaoMax;
+};
+string c3Presidente::getFormacaoMax(){
+    return FormacaoMax;
+};
+
+ 
