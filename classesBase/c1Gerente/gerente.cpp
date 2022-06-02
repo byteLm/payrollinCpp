c30a7308bc024314d3293df32a02f9ca31424aae
Luccas Freitas <90070934+byteLm@users.noreply.github.com>
2022-06-02 00:01:59 -0300

    Create gerente.cpp

---
+++ classesBase/c1Gerente/gerente.cpp
@@ -0,0 +1,24 @@
+
+#include "gerente.h"
+
+c1Gerente::c1Gerente(){
+    this->areaSupervisao = "";
+}
+c1Gerente::c1Gerente(int codigo, string nome, string endereco, string telefone, string dataIngresso, float salario, int desig, string AreaSupervisao){
+    this->codigo = codigo;
+    this->desig = desig;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->telefone = telefone;
+    this->dataIngresso = dataIngresso;
+    this->salario = salario;
+    this->areaSupervisao = AreaSupervisao;
+
+}
+
+void c1Gerente::setAreaSupervisao(string areaSuperfisao){
+    this->areaSupervisao = areaSuperfisao;
+}
+string c1Gerente::getAreaSupervisao(){
+    return areaSupervisao;
+}
