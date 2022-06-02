7d4de4eb10ae911f4e76790c6ab6846bf44af878
Luccas Freitas <90070934+byteLm@users.noreply.github.com>
2022-06-02 00:04:52 -0300

    Create presidente.h

---
+++ classesBase/c3Presidente/presidente.h
@@ -0,0 +1,20 @@
+#include "Classes\c2Diretor\diretor.h"
+
+class c3Presidente : public c2Diretor{
+    
+    public:
+        c3Presidente();
+        c3Presidente(int codigo, string nome, string endereco, string telefone, string dataIngresso, float salario, int desig, string AreaSupervisao, string areaFormacao, string formMax);
+        virtual ~c3Presidente();
+        void setFormacaoMax(string FormacaoMax);
+        string getFormacaoMax();
+        
+    protected:
+        string FormacaoMax;
+    
+    
+    
+};
+
+
+
